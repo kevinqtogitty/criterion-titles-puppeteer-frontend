197e93ff5e7608a7e455796811c974d6628644c4
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2023-05-07 23:34:10 -0400

    Update: added table to display data

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
-interface CriterionFilm {
+export interface CriterionFilm {
   title: string;
   director: string;
   year: string;
@@ -10,17 +10,95 @@ interface CriterionFilm {
 }
 function App() {
   const [allCriterionFilms, setAllCriterionFilms] = useState<CriterionFilm[]>();
+  const [randomTenFilms, setRandomTenFilms] = useState<CriterionFilm[]>();
+  const [isFetching, setIsFetching] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
+
+  console.log(randomTenFilms, allCriterionFilms);
 
   useEffect(() => {
     fetchCriterionFilmData();
   }, []);
 
+  useEffect(() => {
+    if (allCriterionFilms && !sessionStorage.getItem('savedTen')) {
+      generateRandomTen();
+    } else {
+      const data: CriterionFilm[] = JSON.parse(
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        sessionStorage.getItem('savedTen')!
+      );
+      setRandomTenFilms(data);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [allCriterionFilms]);
+
   const fetchCriterionFilmData = async () => {
-    const response = await fetch('http://localhost:8080/films');
-    const data = await response.json();
-    setAllCriterionFilms(data);
+    try {
+      const response = await fetch('http://localhost:8080/films');
+      const data = await response.json();
+      setAllCriterionFilms(data);
+      setIsFetching(false);
+    } catch (error) {
+      console.log(error);
+    }
   };
-  return <div>Hello</div>;
+
+  const randomInteger = (min: number, max: number) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+
+  const generateRandomTen = () => {
+    const randomFilms: CriterionFilm[] = [];
+    if (allCriterionFilms) {
+      for (let i = 0; i < 10; i++) {
+        const generatedNumber = randomInteger(0, allCriterionFilms.length);
+        if (randomFilms.includes(allCriterionFilms[generatedNumber])) {
+          i--;
+        } else {
+          randomFilms.push(allCriterionFilms[generatedNumber]);
+        }
+      }
+    }
+    sessionStorage.setItem('savedTen', JSON.stringify(randomFilms));
+    setRandomTenFilms(randomFilms);
+  };
+
+  return (
+    <div className="main-container">
+      <h1>Random Criterion</h1>
+      <button onClick={generateRandomTen}>New list</button>
+
+      {isFetching ? (
+        <div>Fetching criterion films</div>
+      ) : (
+        <table className="table-main">
+          <thead>
+            <tr className="table-categories">
+              <td>Title</td>
+              <td>Director</td>
+              <td>Year</td>
+              <td>Country</td>
+            </tr>
+          </thead>
+          <tbody>
+            {randomTenFilms?.map((film) => (
+              <a href={`${film.link}`}>
+                <tr className="table-data-row">
+                  <td>
+                    <img src={`${film.imgUrl}`} alt={`${film.title}`} />
+                  </td>
+                  <td>{film.director}</td>
+                  <td>{film.year}</td>
+                  <td>{film.country}</td>
+                </tr>
+              </a>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
 }
 
 export default App;
