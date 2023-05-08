e0d803aaf1b9945c4239b606ee318c100b17fbdf
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2023-05-08 18:48:20 -0400

    Update: added vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { Analytics } from '@vercel/analytics/react';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+    <Analytics mode={'production'} />
+  </React.StrictMode>
+);
